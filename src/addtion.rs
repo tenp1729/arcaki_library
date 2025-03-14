6994567b2c99568725b21ab60a8fabfbd4f05ebe
tenp1729 <rei.kuboi@icloud.com>
2025-03-14 20:35:31 +0900

    git add .

--- src/addtion.rs
+++ src/addtion.rs
@@ -1,5 +1,5 @@
 pub fn get_sqrt(x: i64) -> i64 {
-    let mut res = (x as f64).sqrt() as i64 - 1;
+    let mut res = (x as f64).sqrt() as i64 - 100;
     while res < 0 || (res + 1) * (res + 1) <= x{
         res += 1;
     }
